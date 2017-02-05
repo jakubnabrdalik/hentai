e76f0f94c62904831262997651649683d4ed5cfd
Jakub Nabrdalik <jakubn@gmail.com>
2017-02-05 21:55:25 +0100

    Acceptance Spec
    It's time to update our acceptance spec to next level, and move to the next module

--- hentai/src/test/groovy/eu/solidcraft/AcceptanceSpec.groovy
+++ hentai/src/test/groovy/eu/solidcraft/AcceptanceSpec.groovy
@@ -1,18 +1,37 @@
 package eu.solidcraft
 
 import eu.solidcraft.base.IntegrationSpec
+import eu.solidcraft.film.domain.FilmFacade
+import eu.solidcraft.film.domain.SampleFilms
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.test.context.support.WithMockUser
-import spock.lang.Ignore
+import org.springframework.test.web.servlet.ResultActions
 
-@Ignore
-class AcceptanceSpec extends IntegrationSpec {
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+class AcceptanceSpec extends IntegrationSpec implements SampleFilms {
+    @Autowired
+    FilmFacade filmFacade
 
     @WithMockUser
     def "positive renting scenario"() {
         given: 'inventory has an old film "American Clingon Bondage" and a new release of "50 shades of Trumpet"'
+            filmFacade.add(trumper)
+            filmFacade.add(clingon)
 
         when: 'I go to /films'
+            ResultActions getFilms = mockMvc.perform(get("/films"))
         then: 'I see both films'
+            getFilms.andExpect(status().isOk())
+                .andExpect(content().json("""
+                {
+                    "content": [
+                        {"title":"$clingon.title","type":"$clingon.type"},
+                        {"title":"$trumper.title","type":"$trumper.type"}
+                    ]
+                }"""))
 
         when: 'I go to /points'
         then: 'I see I have no points'
